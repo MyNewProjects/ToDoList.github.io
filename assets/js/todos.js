26155ea04419e7dac9bdd300614bfafabd773745
MyNewProjects <44367339+MyNewProjects@users.noreply.github.com>
2018-10-28 21:09:14 +0530

    Version 3.0
    Add the Comment to HTML , CSS and jQuery files

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,38 +1,54 @@
 //Remove all the task by clicking on the button "Clear All"
 $("#clear").on("click",function(){
+
 	$("#container li").remove();
+
 });
 
 // Check Off Specific Todos By Clicking
 $("#container ul").on("click", "li", function(){
+
 	$(this).toggleClass("completed");
+
 });
 
 //Click on X to delete Todo
 $("#container ul").on("click", "span", function(event){
+
 	$(this).parent().fadeOut(500,function(){
+
 		$(this).remove();
+
 	});
+
 	event.stopPropagation();
 });
 
 // Adding Task On clicking the Buttton "Add Task"
 $(" #addTask").on("click", function(){
+
    var task = $("#kaam").val();
    var date=  $("#tariq").val();
 
    if(task=="" && date == ""){
+
    	 alert("Task Cannot Empty !");
    }
+
    else if(task!="" && date==""){
+
    	$("#container ul").append("<li><span><i class='fa fa-times'></i></span> " + task +"</li>");
+
    	var task = $("#kaam").val("");
    	var date=  $("#tariq").val("");
    }
-   	else{
-   		$("#container ul").append("<li><span><i class='fa fa-times'></i></span> " + task +"  On: "+date + "</li>");
+   else{
+
+   	$("#container ul").append("<li><span><i class='fa fa-times'></i></span> " + task +"  On: "+date + "</li>");
+
 		var task = $("#kaam").val("");
-   		var date=  $("#tariq").val("");
+   	var date=  $("#tariq").val("");
+
    	}
   }); 
 
@@ -42,9 +58,10 @@ $("#toggle-form").click(function(){
 	$("input[type=text]").fadeToggle();
 	$("#tariq").fadeToggle();
 });
-// Click On "+" To Add Sample Task From Another Container
 
+// Click On "+" To Add Sample Task From Another Container
 $("#sampletask li").on("click","span",function(){
- var para = $(this).parent().text();
-$("#container ul").append("<li><span><i class='fa fa-times'></i></span> "+ para +"</li>")  
+
+   var para = $(this).parent().text();
+   $("#container ul").append("<li><span><i class='fa fa-times'></i></span> "+ para +"</li>")  
 });
